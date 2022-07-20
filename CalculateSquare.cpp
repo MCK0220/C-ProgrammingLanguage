c3ba88b8b93644a1dac98774189c7fa23e768e80
mck.studios <99533662+MCK0220@users.noreply.github.com>
2022-07-20 20:55:03 +0300

    Create CalculateSquare.cpp
    calculating the square of a number

---
+++ CalculateSquare.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int number , square;
+
+    std::cout<<"Enter Number: "<<std::endl;
+    std::cin>>number;
+
+    square = number*number;
+
+    std::cout<<"Square of number: "<<square<<std::endl;
+}
