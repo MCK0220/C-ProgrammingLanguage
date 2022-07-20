5a3acd043aca736be50ccc4ecbba527d05ddc6a2
mck.studios <99533662+MCK0220@users.noreply.github.com>
2022-07-20 20:56:53 +0300

    Create SimpleCalculator.cpp
    Simple Calculator in Cpp

---
+++ SimpleCalculator.cpp
@@ -0,0 +1,19 @@
+	#include <iostream>
+
+	int main()
+	{
+	    int number1,number2,total;
+
+	    std::cout<<"Enter First Number: "<<std::endl;
+	        std::cin>>number1;
+
+	    std::cout<<"Enter Second Number: "<<std::endl;
+	        std::cin>>number2;
+
+	    total = number1+number2;
+
+	    std::cout<<"Numbers Total: \t"<<total<<std::endl;
+
+	    return 0;
+
+	}
